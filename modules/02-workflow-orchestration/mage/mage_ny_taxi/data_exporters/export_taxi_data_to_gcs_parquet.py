29f718623bc63825ad6472ed0f9d840a32e6f995
Ronald Fung <rfung.dev@icloud.com>
2024-02-05 16:36:32 +0000

    Clean up module-2 answers

--- modules/02-workflow-orchestration/mage/mage_ny_taxi/data_exporters/export_taxi_data_to_gcs_parquet.py
+++ modules/02-workflow-orchestration/mage/mage_ny_taxi/data_exporters/export_taxi_data_to_gcs_parquet.py
@@ -17,34 +17,16 @@ if 'data_exporter' not in globals():
 
 @data_exporter
 def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
-    """
-    Template for exporting data to a Google Cloud Storage bucket.
-    Specify your configuration settings in 'io_config.yaml'.
-
-    Docs: https://docs.mage.ai/design/data-loading#googlecloudstorage
-    """
-
-
-    # config_path = path.join(get_repo_path(), 'io_config.yaml')
-    # config_profile = 'dev'
 
     bucket_name = 'module-2-bucket-636e6c4b0a8b7d88'
-    project_id = 'dtc-de-course-410021'
 
-    table_name = 'nyc_taxi_data_1'
-    # object_key = 'nyc_taxi_data.parquet'
+    table_name = 'nyc_taxi_data'
 
     root_path = f'{bucket_name}/{table_name}'
 
     table = pa.Table.from_pandas(df)
 
     gcs = pa.fs.GcsFileSystem()
-
-    # GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
-    #     df,
-    #     bucket_name,
-    #     object_key,
-    # )
 
     pq.write_to_dataset(
         table,
@@ -55,4 +37,3 @@ def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
 
     print(f"Expected number of partitions: {df.lpep_pickup_date.nunique()}")
     print(f"Exported {len(pq.ParquetDataset(root_path, filesystem=gcs).files)} files to gcs")
-    # print(f"Exported files: {pq.ParquetDataset(root_path, filesystem=gcs).files}")
--- modules/02-workflow-orchestration/mage/mage_ny_taxi/data_exporters/export_taxi_data_to_postgres.py
+++ modules/02-workflow-orchestration/mage/mage_ny_taxi/data_exporters/export_taxi_data_to_postgres.py
@@ -10,12 +10,7 @@ if 'data_exporter' not in globals():
 
 @data_exporter
 def export_data_to_postgres(df: DataFrame, **kwargs) -> None:
-    """
-    Template for exporting data to a PostgreSQL database.
-    Specify your configuration settings in 'io_config.yaml'.
-
-    Docs: https://docs.mage.ai/design/data-loading#postgresql
-    """
+    
     schema_name = 'mage'  # Specify the name of the schema to export data to
     table_name = 'green_taxi'  # Specify the name of the table to export data to
     config_path = path.join(get_repo_path(), 'io_config.yaml')
